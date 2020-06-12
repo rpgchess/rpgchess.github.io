d3347ac4373f2956e682db5084a595b16d1815c7
Claudio A. Martins <claudio.almeida@linx.com.br>
2020-06-12 01:29:24 -0300

    update profile section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var navBtnWidth = $('.navbar a').css('width');
 
+var github = [];
+
 $('.navbar a').mouseenter(e => {
   $(e.target).clearQueue();
   $(e.target).animate({
@@ -15,6 +17,41 @@ $('.navbar a').mouseout(e => {
   }, 300);
 });
 
-$(document).ready(function(){
-  $('.slicker').slick();
-});
+function loadRepositories() {
+  $.get( "https://api.github.com/users/rpgchess/repos", function(data) {
+    data.forEach(function(repo, index, repos) {
+      github.push({
+        name: repo.name,
+        href: repo.html_url,
+        description: repo.description,
+        language: repo.language
+      });
+    });
+    addProfiles();
+  });
+}
+
+loadRepositories();
+
+function addProfiles() {
+  var slide = document.getElementById('github');
+  var html = '<h1>Portfolio</h1>';
+      html +='<div class="row">';
+
+  github.forEach(function(repo, index, repos) {
+    repo.description = (repo.description == null)? '...' : repo.description;
+    repo.language = (repo.language == null)? '' : repo.language;
+    html += '<div class="col mb-2 col-sm-2 col-md-4 col-lg-4">'
+    html += '<div class="card border-dark" style="height:92%">';
+    html += '<a href="' + repo.href + '" target="_blank">';
+    html += '<div class="card-header font-weight-bold text-left text-uppercase small">' + repo.name + '</div>';
+    html += '<div class="card-text mt-2 text-center"><p>' + repo.description + '</p></div>';
+    html += '<div class="card-footer text-uppercase text-muted small d-flex justify-content-end">' + repo.language + '</div>';
+    html += '</a>';
+    html += '</div>';
+    html += '</div>';
+  });
+
+  html += '</div>';
+  slide.innerHTML += html;
+}
